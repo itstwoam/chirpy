9830becb5b8dc11dba6e52749a5f296d4a6fc01a
itstwoam <github@itstwoam.com>
2025-09-08 18:57:54 -0400

    Adding counters to pages

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,10 +5,25 @@ import (
 	"fmt"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareInc(next http.Handler) http.Handler {
+	cfg.filserverHits.Add(1)	
+}
+
+func serveHits(w http.ResponseWrite, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(strconv.Itoa())
+}
+
 func StartServer() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./internal/app"))
 	mux.Handle("/app/", http.StripPrefix("/app", fs))
+	mux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app", fs)))
 	mux.HandleFunc("/healthz", serveStatus)
 	server := http.Server{}
 	server.Handler = mux
